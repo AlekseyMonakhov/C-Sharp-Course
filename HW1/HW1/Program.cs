ae675193fb55193db3b6005491c3dbf6cf4e7adb
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-02-19 22:12:58 +0200

    Add project files.

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Less2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first operand");
+            string firstOperand = Console.ReadLine();
+
+            Console.WriteLine("enter operation");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("enter second operand");
+            string secondOperand = Console.ReadLine();
+
+
+
+            Console.WriteLine("is reversed");
+            string isReverse = Console.ReadLine();
+
+
+
+
+            Console.WriteLine(Calculate(float.Parse(firstOperand), float.Parse(secondOperand), operation, bool.Parse(isReverse)));
+        }
+        static float Calculate(float firstOperant, float secndOperant, string operation, bool isRevers = false)
+        {
+            if (operation == "+")
+            {
+                return firstOperant + secndOperant;
+            }
+
+            if (operation == "-")
+            {
+                return firstOperant - secndOperant;
+            }
+
+            if (operation == "/")
+            {
+                return isRevers ? secndOperant / firstOperant : firstOperant / secndOperant;
+            }
+
+            return firstOperant * secndOperant;
+        }
+
+
+    }
+}
